71201c6af527ea828896fc692019ef36778cf3e5
fizzfuzzhk <fizzfuzzhk@gmail.com>
2020-08-30 07:28:03 +0000

    7th

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -19,8 +19,54 @@ describe('total likes', () => {
         }
     ]
 
-    test.only('when list has only one blog, equals the likes of that', () => {
+    test('when list has only one blog, equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
         expect(result).toBe(5)
+        })
+})
+
+describe('most likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 2,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 8,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 4,
+            __v: 0
+        }
+    ]
+
+    test.only('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        expect(result).toEqual({
+            "author": "Edsger W. Dijkstra",
+            "likes": 8,
+            "title": "Go To Statement Considered Harmful"
+        })
         })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,14 +2,38 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = (blogs) => {
-    sum = blogs[0].likes
-    for_debug = 1
+const totalLikes = (blogs) => { 
+    var sum = 0
+    for (var i=0 ; i < blogs.length; i++){
+        sum += blogs[i].likes
+    }
     return sum
 }
 
+const favoriteBlog = (blogs) => {
+    var max = 0
+    var current = 0
+    var max_list = 0
+    
+    for (var i=0 ; i < blogs.length; i++){
+        current = blogs[i].likes
+        if (current === Math.max(max,current)){
+            max_list = i
+            max = current
+        }
+    }
+
+    result = {title :blogs[max_list].title,
+        author :blogs[max_list].author,
+        likes:blogs[max_list].likes
+        }
+    return  result
+}
+
+
 module.exports = {
     dummy,
-   totalLikes
+   totalLikes,
+   favoriteBlog
 }
 

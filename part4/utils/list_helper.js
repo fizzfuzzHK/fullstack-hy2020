5122c453a6126ae72bdf0847ea0f1a8dd422c91a
fizzfuzzhk <fizzfuzzhk@gmail.com>
2020-08-30 06:51:23 +0000

    6th

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,15 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    sum = blogs[0].likes
+    for_debug = 1
+    return sum
+}
+
+module.exports = {
+    dummy,
+   totalLikes
+}
+
